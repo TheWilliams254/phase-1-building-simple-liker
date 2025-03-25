776deea68335ff5b91de1ee364994c0ef7a2b5fc
william <williamwambugu663@gmail.com>
2025-03-25 07:06:24 +0300

    hidden class

--- main.js
+++ main.js
@@ -3,10 +3,35 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+document.addEventListener("DOMContentLoaded", () => {
+  // Select all heart elements
+  const hearts = document.querySelectorAll(".like-glyph");
+  const errorModal = document.getElementById("modal");
+  const errorMessage = document.getElementById("modal-message");
 
+  errorModal.classList.add("hidden");
 
-
-
+  hearts.forEach((heart) => {
+    heart.addEventListener("click", () => {
+      // Simulating a server request
+      mimicServerCall()
+        .then(() => {
+          if (heart.textContent === EMPTY_HEART) {
+            heart.textContent = FULL_HEART;
+            heart.classList.add("activated-heart");
+          } else {
+            heart.textContent = EMPTY_HEART;
+            heart.classList.remove("activated-heart");
+          }
+        })
+        .catch((error) => {
+          errorMessage.textContent = error;
+          errorModal.classList.remove("hidden");
+          setTimeout(() => errorModal.classList.add("hidden"), 3000);
+        });
+    });
+  });
+});
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
